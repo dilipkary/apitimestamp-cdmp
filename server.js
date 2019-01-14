9939ba0d00379d95ebcc435aee0716934d07431b
Glitch (important-watchmaker) <none>
2019-01-14 07:11:53 +0000

    🌃💓 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var time = require("./time.js");
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+app.get('/api/timestamp/:time',function(req,res){
+    var data = req.params;
+    // console.log(data);
+    res.json(time(data.time));
+});
+app.use('/',express.static(__dirname+'/public'));
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
